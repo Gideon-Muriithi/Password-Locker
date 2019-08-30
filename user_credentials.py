e7a5a2377331eae5b5c2ec5ed6cef9cc5b1d024e
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-30 16:47:12 +0300

    This commit adds method the defines the properties of a user instance

--- user_credentials.py
+++ user_credentials.py
@@ -18,4 +18,11 @@ class Credentials:
         for user in User.users_list:
             if (user.fname == fname and password == password):
                 current_user = user.fname
-        return current_user        
+        return current_user
+
+    def __init__(self, user_name, site_name, account_name, password):
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password
+        
