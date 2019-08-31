f1fcdd60f1a02a2123afb7e5df38f9900887d025
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-31 10:33:36 +0300

    This commit adds a function that sets user account before tests are executed

--- credential_test.py
+++ credential_test.py
@@ -3,3 +3,5 @@ import unittest
 from user_credentials import User, Credentials
 
 class TestUser(unittest.TestCase):
+    def createAcc(self):
+        self.new_user = User("Gideon", "Chef", "Been2000")
