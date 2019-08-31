1f243b9308ae28ff20c096d131737ff390a9dc53
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-31 11:00:26 +0300

    This commit adds a method that checks if intialization of user instances is properly done

--- credential_test.py
+++ credential_test.py
@@ -6,8 +6,17 @@ class TestUser(unittest.TestCase):
     def createAcc(self):
         self.new_user = User("Gideon", "Chef", "Been2000")
 
+    def test__init__(self):
+        self.assertEqual(self.new_user.fname, "Gideon")
+        self.assertEqual(self.new_user.lname, "Chef")    
+        self.assertEqual(self.new_user.password, "Been2000")    
+    
     def test_save_user(self):
         self.new_user.save_user()
         self.assertEqual(len(User.users_list), 1) 
 
-class TestCredentials(unittest.TestCase):           
+# class TestCredentials(unittest.TestCase):
+#     def test_user_credentials(self):
+#     self.new_user = User("Gideon", "Chef", "Been2000")
+
+               
