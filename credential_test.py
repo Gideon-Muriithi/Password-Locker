de9ee73172669345d0c42908b5c436cf08a3086b
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-31 23:41:04 +0300

    This commit removes bugs and adds docstrings in the code

--- credential_test.py
+++ credential_test.py
@@ -2,81 +2,119 @@ import pyperclip
 import unittest
 from user_credentials import User, Credentials
 
+
 class TestUser(unittest.TestCase):
-    def createAcc(self):
+    '''
+    Test class that defines test cases for the User class behaviours
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before each test cases is executed
+        '''
         self.new_user = User("Gideon", "Chef", "Been2000")
 
-    def test__init__(self):
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_user.fname, "Gideon")
-        self.assertEqual(self.new_user.lname, "Chef")    
-        self.assertEqual(self.new_user.password, "Been2000")    
-    
+        self.assertEqual(self.new_user.lname, "Chef")
+        self.assertEqual(self.new_user.password, "Been2000")
+
     def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user_list
+        '''
         self.new_user.save_user()
-        self.assertEqual(len(User.users_list), 1) 
+        self.assertEqual(len(User.user_list), 1)
+
 
 class TestCredentials(unittest.TestCase):
-    def test_user_check(self):
+    '''
+    Test class that defines test cases for the Crededentials class behaviours
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def test_check_credentials(self):
+        '''
+		test_check_credentials to test whether user_check method executes as intended
+		'''
         self.new_user = User("Gideon", "Chef", "Been2000")
         self.new_user.save_user()
         addUser = User("Peter", "Chamgei", "Double10")
         addUser.save_user()
-        for user in User.users_list:
+
+        for user in User.user_list:
             if user.fname == addUser.fname and user.password == addUser.password:
                 current_user = user.fname
         return current_user
-        self.assertEqual(current_user, Credentials.user_check(addUser.password, addUser.fname))
+
+        self.assertEqual(current_user, Credentials.user_check(
+            addUser.password, addUser.fname))
 
     def setUp(self):
-		self.new_credential = Credentials("Gideon","Twitter","@gideonm","Been2000")
-    
-    def test__init__(self):
-        self.assertEqual(self.new_credential.username,"Gideon")
-		self.assertEqual(self.new_credential.site_name, "Twitter")
-		self.assertEqual(self.new_credential.account_name, "@gideonm")
-		self.assertEqual(self.new_credential.password, "Been2000")    
-    
+        '''
+		setUp method to create an account's credentials before each test
+		'''
+        self.new_credential = Credentials(
+            "Gideon", "Twitter", "@gideonm", "Been2000")
+
+    def test_init(self):
+        '''
+		test_init to test if the initialization of credential instances is properly done
+		'''
+        self.assertEqual(self.new_credential.username, "Gideon")
+        self.assertEqual(self.new_credential.site_name, "Twitter")
+        self.assertEqual(self.new_credential.account_name, "@gideonm")
+        self.assertEqual(self.new_credential.password, "Been2000")
+
     def test_save_credentials(self):
-		self.new_credential.save_credentials()
-		Facebook = Credential("Grace", "Facebook", "@graceg",'Been2000')
-		Facebook.save_credentials()
-		self.assertEqual(len(Credentials.credentials_list),2)
+        '''
+		test_save_credentials to test if the new credentials are saved into the credentials list
+		'''
+        self.new_credential.save_credentials()
+        Facebook = Credentials("Grace", "Facebook", "@graceg", 'Been2000')
+        Facebook.save_credentials()
+        self.assertEqual(len(Credentials.user_credentials_list), 2)
 
     def tearDown(self):
-		Credentials.credentials_list = []
-		User.users_list = []    
-    
+        Credentials.user_credentials_list = []
+        User.user_list = []
+
     def test_display_credentials(self):
-		self.new_credential.save_credentials()
-		Facebook = Credential("Grace", "Facebook", "@graceg",'Been2000')
-		Facebook.save_credentials()
-		gmail = Credentials("Grace",'Gmail', "graceg", "Been2000")
-		gmail.save_credentials()
-		self.assertEqual(len(Credentials.display_credentials(Facebook.username)),2)
+        self.new_credential.save_credentials()
+        Facebook = Credentials("Grace", "Facebook", "@graceg", 'Been2000')
+        Facebook.save_credentials()
+        gmail = Credentials("Grace", 'Gmail', "graceg", "Been2000")
+        gmail.save_credentials()
+        self.assertEqual(
+            len(Credentials.display_credentials(Facebook.username)), 2)
 
     def test_get_by_site_name(self):
-		self.new_credential.save_credentials()
-		Facebook = Credential("Grace", "Facebook", "@graceg",'Been2000')
-		Facebook.save_credentials()
-		credential_exists = Credentials.get_by_site_name("Facebook")
-		self.assertEqual(credential_exists, Facebook) 
+        self.new_credential.save_credentials()
+        Facebook = Credentials("Grace", "Facebook", "@graceg", 'Been2000')
+        Facebook.save_credentials()
+        credential_exists = Credentials.get_by_site_name("Facebook")
+        self.assertEqual(credential_exists, Facebook)
 
     def test_copy_credential(self):
-		self.new_credential.save_credentials()
-		Facebook = Credential("Grace", "Facebook", "@graceg",'Been2000')
-		Facebook.save_credentials()
-		get_credential = None
-		for credential in Credentials.user_credentials_list:
-				get_credential = Credentials.get_by_site_name(credential.site_name)
-				return pyperclip.copy(get_credential.password)
-		Credentials.copy_credential(self.new_credential.site_name)
-		self.assertEqual("Been200", pyperclip.paste())
-		print(pyperclip.paste())   
-            
+        self.new_credential.save_credentials()
+        Facebook = Credentials("Grace", "Facebook", "@graceg", 'Been2000')
+        Facebook.save_credentials()
+        get_credential = None
+        for credential in Credentials.user_credentials_list:
+            get_credential = Credentials.get_by_site_name(credential.site_name)
+            return pyperclip.copy(get_credential.password)
+        Credentials.copy_credential(self.new_credential.site_name)
+        self.assertEqual("Been200", pyperclip.paste())
+        print(pyperclip.paste())
+
+
 if __name__ == '__main__':
-	unittest.main(verbosity=2)
-
-
-
-               
-    
+    unittest.main()
--- user_credentials.py
+++ user_credentials.py
@@ -2,59 +2,91 @@ import pyperclip
 import random
 import string
 
+
 class User:
-    users_list = []
+    """
+    Class that generates new instances of users
+    """
+
+    user_list = []
+
     def __init__(self, fname, lname, password):
+        '''
+		__init__ method that define the properties each user object will have
+		'''
+
         self.fname = fname
         self.lname = lname
         self.password = password
 
-    def user_saving:
-        user.users_list.append(self)    
+    def save_user(self):
+        '''
+        save_contact method that saves contact objects into contact_list
+        '''
+        User.user_list.append(self)
+
 
 class Credentials:
     credentials_list = []
-    user_credentials = []
+    user_credentials_list = []
 
     @classmethod
     def user_check(cls, fname, password):
+        '''
+		user_check method that checks if the name and password entered match entries in the users_list
+		'''
         current_user = ''
-        for user in User.users_list:
-            if (user.fname == fname and password == password):
+        for user in User.user_list:
+            if user.fname == fname and password == password:
                 current_user = user.fname
         return current_user
 
     def __init__(self, username, site_name, account_name, password):
+        '''
+		__init__ method that define the properties each credentials object will have
+		'''
         self.username = username
         self.site_name = site_name
         self.account_name = account_name
         self.password = password
 
-    def credentials_saving(self): 
-        Credentials.crededentials_list.append(self)
+    def save_credentials(self):
+        '''
+		save_credentials method to save a newly created credentials instances
+		'''
+        Credentials.user_credentials_list.append(self)
 
-    def password_generation(size = 8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
-        pwd_gen = ''.join(random.choice(char) for _ in range(size))
-		return pwd_gen
-   
+    def pwd_generation(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+        '''
+		pwd_generation method to generate password for credentials instances
+		'''
+        pwd_gen = " ".join(random.choice(char) for _ in range(size))
+        return pwd_gen
+
     @classmethod
     def display_credentials(cls, username):
+        '''
+		Class method to display the list of credentials saved
+		'''
         user_credentials_list = []
         for credential in user_credentials_list:
-            if (credential.username == username):
+            if credential.username == username:
                 user_credentials_list.append(credential)
         return user_credentials_list
 
     @classmethod
     def get_by_site_name(cls, site_name):
-        for credential in cls.crededentials_list:
-            if (credential.site_name == site_name):
-                user_credentials_list.append(credential)
+        '''
+		Method that takes in a site_name and returns a credential that matches that site_name
+		'''
+        for credential in cls.credentials_list:
+            if credential.site_name == site_name:
         return credential
 
     @classmethod
     def copy_credential(cls, site_name):
+        '''
+	    method that copies a credential's info after the credential's site name is entered
+	    '''
         get_credential = Credentials.get_by_site_name(site_name)
         return pyperclip.copy(get_credential.password)
-
-
