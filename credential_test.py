4f6f2708fb39276790358cbe15ae2b99c96483ed
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-31 10:43:59 +0300

    This commit a test method that checks if users details are successfully saved in the list

--- credential_test.py
+++ credential_test.py
@@ -5,3 +5,7 @@ from user_credentials import User, Credentials
 class TestUser(unittest.TestCase):
     def createAcc(self):
         self.new_user = User("Gideon", "Chef", "Been2000")
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list), 1)    
