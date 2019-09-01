2830c9bd3cf1245f473b41cfb54f24bdd79035af
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-01 21:40:13 +0300

    This commit adds function for create users and another for saving users and also import various modules in pwd locker file

---
+++ pwd_locker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+import pyperclip
+from user_credentials import User, Credentials
+
+
+def create_user(fname, lname, password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname, lname, password)
+	return new_user
+
+
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)
+
