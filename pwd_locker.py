4eb61c7ac416029b5b0cf74abf85575b5699ce70
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-02 08:26:15 +0300

    This commit adds prints functions to get messages to the user and also creates a while loop

--- pwd_locker.py
+++ pwd_locker.py
@@ -36,5 +36,34 @@ def main():
 		new_credential = Credentials(username,site_name,account_name,password)
 		return new_credential
 
+	def save_credentials(credential):
+		'''
+		Function to save a newly created credential
+		'''
+		Credentials.save_credentials(credential)
+
+	def display_credentials(username):
+		'''
+		Function to display user credentials
+		'''
+		return Credentials.display_credentials(username)
+
+	def copy_credential(site_name):
+		'''
+		Function to copy credential details to the clipboard
+		'''
+		return Credentials.copy_credential(site_name)
+
+	print(" ")
+	print("Hi there! Welcome to Password Locker App!")
+	while True:
+		print(" ")
+		print("-"*60)
+		print("Use these codes to navigate: \n CA-Create Account \n LI-Log In \n Ex-Exit")
+		short_code = input("Enter a choice: ").lower().strip()
+		if short_code == "Ex":
+			break	
+				
+
 if __name__ == '__main__':
 	main()
