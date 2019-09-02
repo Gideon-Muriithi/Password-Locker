daef3af45a02961a4e83ec8beabe0d84b3de41a4
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-02 08:08:25 +0300

    This commit create a main function and also a create credentials account

--- pwd_locker.py
+++ pwd_locker.py
@@ -1,19 +1,40 @@
-#!/usr/bin/env python3.6
-import pyperclip
-from user_credentials import User, Credentials
+def main():
+	def create_user(fname, lname, password):
+		'''
+		Function to create a new user account
+		'''
+		new_user = User(fname, lname, password)
+		return new_user
 
 
-def create_user(fname, lname, password):
-	'''
-	Function to create a new user account
-	'''
-	new_user = User(fname, lname, password)
-	return new_user
+	def save_user(user):
+		'''
+		Function to save a new user account
+		'''
+		User.save_user(user)
 
 
-def save_user(user):
-	'''
-	Function to save a new user account
-	'''
-	User.save_user(user)
+	def verify_user(fname, password):
+		'''
+		Function to verify user exist before creating the user credentials
+		'''
+		user_verification = Credentials.user_check(fname, password)
+		return user_verification
 
+
+	def pwd_generation():
+		'''
+		pwd_generation function to generate a password automatically
+		'''
+		pwd_gen = Credentials.pwd_generation()
+		return pwd_gen
+
+	def create_credentials(username,site_name,account_name,password):
+		'''
+		Function that create new credential
+		'''
+		new_credential = Credentials(username,site_name,account_name,password)
+		return new_credential
+
+if __name__ == '__main__':
+	main()
